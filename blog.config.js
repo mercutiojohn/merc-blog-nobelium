cc784ce351b3797367d363aaf7c490c7f767d871
Mercutio <mercutio_john@foxmail.com>
2022-04-22 19:53:32 +0800

    test

--- blog.config.js
+++ blog.config.js
@@ -25,25 +25,25 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-44SCYY00E5' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'gitalk', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: '', // The repository of store comments
-      owner: '',
-      admin: [],
-      clientID: '',
-      clientSecret: '',
+      repo: 'mercblog-comments', // The repository of store comments
+      owner: 'mercutiojohn',
+      admin: ['mercutiojohn'],
+      clientID: '5ea89b56d4798e1d472d',
+      clientSecret: '6ea23c80ec0e7dea1b6e6834e50890cd729856bf',
       distractionFreeMode: false
     },
     utterancesConfig: {
