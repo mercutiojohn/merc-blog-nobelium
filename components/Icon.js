97b66ac41079acfdb2bab4c1cd7b1ff6d65a4c39
mercutio <gycabc123@qq.com>
2022-04-25 15:41:59 +0800

    test icon

--- components/Icon.js
+++ components/Icon.js
@@ -27,7 +27,7 @@ const Icon = () => {
     //     </radialGradient>
     //   </defs>
     // </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 170.08 170.08">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 170.08 170.08">
       <g>
         <g>
           <g clipPath="url(#clipPath)">
