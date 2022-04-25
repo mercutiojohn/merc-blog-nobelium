0a2658c5b85935561dd603192e87cceca7ba7b3c
mercutio <gycabc123@qq.com>
2022-04-25 15:38:07 +0800

    test icon

--- components/Icon.js
+++ components/Icon.js
@@ -30,28 +30,28 @@ const Icon = () => {
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 170.08 170.08">
       <g>
         <g>
-          <g clip-path="url(#clip-path)">
+          <g clipPath="url(#clipPath)">
             <rect fill="#fff" width="170.08" height="170.08" />
-            <path fill-rule="evenodd" fill="#f6e070"
+            <path fillRule="evenodd" fill="#f6e070"
               d="M167.06,86.28a82.67,82.67,0,0,0-20.13-21.94C131,52.25,110.65,45.6,89.66,45.6a103.24,103.24,0,0,0-20.22,2C44.76,52.53,23.26,65.82,8.72,85.26q-2.73,3.64-5,7.46L0,99.54v70.79H170.08V91.61Z"
               transform="translate(0 -0.25)" />
-            <path fill-rule="evenodd" fill="#fff"
+            <path fillRule="evenodd" fill="#fff"
               d="M42.11,88.76A13.08,13.08,0,0,1,52.24,73.45h0A12.74,12.74,0,0,1,67.09,83.89l5.62,30.69a13.09,13.09,0,0,1-10.13,15.31h0a12.73,12.73,0,0,1-14.84-10.44Z"
               transform="translate(0 -0.25)" />
-            <path fill-rule="evenodd" fill="#fff"
+            <path fillRule="evenodd" fill="#fff"
               d="M79.78,80.71A13.09,13.09,0,0,1,89.91,65.4h0a12.74,12.74,0,0,1,14.85,10.44l5.62,30.7a13.09,13.09,0,0,1-10.13,15.31h0A12.74,12.74,0,0,1,85.41,111.4Z"
               transform="translate(0 -0.25)" />
-            <path fill="#221714" fill-rule="#221714"
+            <path fill="#221714" fillRule="#221714"
               d="M90.12,80.42A7.61,7.61,0,0,1,96,71.52h0a7.4,7.4,0,0,1,8.63,6.07l2.21,12.07a7.6,7.6,0,0,1-5.89,8.9h0a7.41,7.41,0,0,1-8.63-6.07Z"
               transform="translate(0 -0.25)" />
-            <path fill="#221714" fill-rule="#221714"
+            <path fill="#221714" fillRule="#221714"
               d="M52.68,88.87A7.61,7.61,0,0,1,58.57,80h0A7.4,7.4,0,0,1,67.2,86l2,11a7.62,7.62,0,0,1-5.89,8.91h0a7.4,7.4,0,0,1-8.63-6.08Z"
               transform="translate(0 -0.25)" />
           </g>
         </g>
       </g>
       <defs>
-        <clipPath id="clip-path" transform="translate(0 -0.25)">
+        <clipPath id="clipPath" transform="translate(0 -0.25)">
           <rect fill="none" x="0.69" width="170.08" height="170.08" rx="39.4" />
         </clipPath>
       </defs>
