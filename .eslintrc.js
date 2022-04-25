b98dff5e24a6de5d3a6a4960f2deb5c2559f7132
mercutio <gycabc123@qq.com>
2022-04-25 17:13:35 +0800

    test disable eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,9 @@ module.exports = {
     }
   },
   rules: {
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
   },
   globals: {
     React: true
